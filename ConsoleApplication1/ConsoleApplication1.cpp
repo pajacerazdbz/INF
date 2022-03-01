111ed5d1e432cff38114b7e673c52b24bc3c3b43
pajacerazdbz <nmzcinfo@gmail.com>
2022-03-01 11:05:25 +0100

    essa

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,32 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    unsigned int n, lp, p, d;
+    bool t;
+
+    cin >> n;
+    lp = 0;
+    p = 2;
+    while (lp < n)
+    {
+        t = true;
+        for (d = 2; d < p; d++)
+            if (p % d == 0)
+            {
+                t = false;
+                break;
+            }
+        if (t)
+        {
+            cout << p << " ";
+            lp++;
+        }
+        p++;
+    }
+    cout << endl;
+    return 0;
+}
